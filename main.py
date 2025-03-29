c1a067b25f8f5a8b6fe895a474a1ff76ea9a4d87
Pranav Anand24 <156640323+PranavAnand2005@users.noreply.github.com>
2025-03-29 16:51:58 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import csv
-
-df = pd.read_csv("SuperStore_Sales_Dataset.csv")
-dn = pd.json_normalize(df)
-display = pd.set_option("dsplay.max.columns",None)
-print(dn)
-print(display)
